fe8dc31434a007a754376711cd5da3c96413f7c6
Manuel <yesas12345678@gmail.com>
2026-06-24 16:43:20 +0200

    chore: change AdSense loading strategy to beforeInteractive

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -46,7 +46,7 @@ export default function RootLayout({
           async
           src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6737064906428173"
           crossOrigin="anonymous"
-          strategy="afterInteractive"
+          strategy="beforeInteractive"
         />
         {children}
       </body>
